6aa1311fd66c8fa9fb686ade9c1a0abcf3e995f1
SuoNi <75001314+chan1919@users.noreply.github.com>
2025-06-01 22:18:33 +0800

    app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,30 @@
-import "./App.css";
 import React from "react";
+import { ConfigProvider, theme } from "antd";
+import zhCN from "antd/lib/locale/zh_CN";
+import { TodoProvider } from "./context/TodoContext";
+import MainLayout from "./layouts/MainLayout";
+import Todos from "./components/Todos";
+import "./App.css";
 
 const App: React.FC = () => {
-  return <div>Hello World!</div>;
+  return (
+    <ConfigProvider
+      locale={zhCN}
+      theme={{
+        token: {
+          colorPrimary: "#1890ff",
+          borderRadius: 10,
+        },
+        algorithm: theme.defaultAlgorithm,
+      }}
+    >
+      <TodoProvider>
+        <MainLayout>
+          <Todos />
+        </MainLayout>
+      </TodoProvider>
+    </ConfigProvider>
+  );
 };
 
 export default App;
