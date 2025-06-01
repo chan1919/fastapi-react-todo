88c23b2b4587abf0592f5e2d2013aa13cfb4783b
SuoNi <75001314+chan1919@users.noreply.github.com>
2025-06-01 22:02:08 +0800

    Header

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Layout, Typography, theme } from "antd";
+import { CheckCircleOutlined } from "@ant-design/icons";
+import "./Header.css";
+
+const { Header } = Layout;
+const { Title } = Typography;
+
+const AppHeader: React.FC = () => {
+  const { token } = theme.useToken();
+
+  return (
+    <Header
+      className="app-header"
+      style={{ backgroundColor: token.colorPrimary }}
+    >
+      <div className="header-content">
+        <CheckCircleOutlined className="header-icon" />
+        <Title level={3} className="header-title">
+          待办事项管理
+        </Title>
+      </div>
+    </Header>
+  );
+};
+
+export default AppHeader;
