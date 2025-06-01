936ad7c25c8100f8ecdec2843d083bc481bb3c03
SuoNi <75001314+chan1919@users.noreply.github.com>
2025-06-01 22:02:21 +0800

    layout

---
+++ frontend/src/layouts/MainLayout.tsx
@@ -0,0 +1,21 @@
+import React, { type ReactNode } from "react";
+import { Layout } from "antd";
+import Header from "../components/Header";
+import "./MainLayout.css";
+
+const { Content } = Layout;
+
+interface MainLayoutProps {
+  children: ReactNode;
+}
+
+const MainLayout: React.FC<MainLayoutProps> = ({ children }) => {
+  return (
+    <Layout className="main-layout">
+      <Header />
+      <Content className="main-content">{children}</Content>
+    </Layout>
+  );
+};
+
+export default MainLayout;
